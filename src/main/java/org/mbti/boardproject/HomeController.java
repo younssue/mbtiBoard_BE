86ef1d0818948187e4d025144c4e982871899bac
younssue <love_rvd7@naver.com>
2024-02-16 15:56:26 +0900

    :art: add test controller

--- src/main/java/org/mbti/boardproject/HomeController.java
+++ src/main/java/org/mbti/boardproject/HomeController.java
@@ -1,19 +1,16 @@
 package org.mbti.boardproject;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/test")
-public class TestController {
-
+@RequestMapping("/home")
+public class HomeController {
     @GetMapping
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Hello, Test!");
+    public String home() {
+        return "redirect:test";
     }
 }
