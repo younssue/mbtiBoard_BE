5b2ae1aff9fa26cde350d1a7207a8316fd72e9db
younssue <love_rvd7@naver.com>
2024-02-16 15:57:58 +0900

    :art: add test controller

--- src/main/java/org/mbti/boardproject/HomeController.java
+++ src/main/java/org/mbti/boardproject/HomeController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String home() {
-        return "redirect:home";
+        return "home";
     }
 }
